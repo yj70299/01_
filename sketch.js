d5c663bef9642249ee510f5bd0b518f155200060
yj7029 <92070320+yj7029@users.noreply.github.com>
2021-10-07 12:44:50 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ let button_4;
 let c;
 
 function setup() {
-    createCanvas(800, 1000);
+    createCanvas(800, 800);
     background(255);
     noStroke();
 }
@@ -20,7 +20,7 @@ function draw() {
   fill(225, 0 , 0);
   rect(0, 200, 200, 200);
 
-  fill(200, 200, 200);
+  fill(255, 255, 0);
   rect(0, 400, 200, 200);
 
   fill(0, 0, 255);
@@ -33,21 +33,21 @@ function draw() {
           button_3 = false;
           button_4 = false;
       }
-  }else if (mouseX < width * 0.25 && mouseY > height * 0.2 && mouseY < height * 0.6) {
+  }else if (mouseX < width * 0.25 && mouseY > height * 0.2 && mouseY < height * 0.4) {
       if (mouseIsPressed) {
           button_1 = false;
           button_2 = true;
           button_3 = false;
           button_4 = false;
       }
-  }else if (mouseX < width * 0.25 && mouseY > height * 0.6 && mouseY < height * 0.8) {
+  }else if (mouseX < width * 0.25 && mouseY > height * 0.4 && mouseY < height * 0.6) {
     if (mouseIsPressed) {
         button_1 = false;
         button_2 = false;
         button_3 = true;
         button_4 = false;
     }
-}else if (mouseX < width * 0.25 && mouseY > height * 0.8 && mouseY < height) {
+}else if (mouseX < width * 0.25 && mouseY > height * 0.6 && mouseY < height) {
     if (mouseIsPressed) {
         button_1 = false;
         button_2 = false;
@@ -65,7 +65,7 @@ if (button_1){
 }else if (button_2){
     c= color(225, 0, 0);
 }else if (button_3){
-    c = color(255, 255, 255);
+    c = color(255, 255, 0);
 }else if (button_4){
     c = color(0, 0, 255);
 }
